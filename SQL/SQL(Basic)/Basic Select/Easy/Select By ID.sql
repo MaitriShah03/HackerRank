d87927332a2b7d15d4f712c369b0864da7833da2
MaitriShah03 <108712031+MaitriShah03@users.noreply.github.com>
2024-02-22 15:29:06 +0530

    Select By ID.sql
    Query all columns for a city in CITY with the ID 1661.

---
+++ SQL/SQL(Basic)/Basic Select/Easy/Select By ID.sql
@@ -0,0 +1,9 @@
+/* Query all columns for a city in CITY with the ID 1661. */
+
+
+/* This query selects all columns/attributes from 'CITY' table */
+SELECT *
+/* Specifies the table from which to retreive the data */
+FROM CITY 
+/* Filter the rows to include those column where ID = 1661 */
+WHERE ID = 1661;
