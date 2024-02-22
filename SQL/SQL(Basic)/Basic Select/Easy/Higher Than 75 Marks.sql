c5503b8db3724e6ca30f31d90c81888ad418c719
MaitriShah03 <108712031+MaitriShah03@users.noreply.github.com>
2024-02-22 19:02:07 +0530

    Higher Than 75 Marks.sql
    Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.

--- SQL/SQL(Basic)/Basic Select/Easy/Higher Than 75 Marks.sql
+++ SQL/SQL(Basic)/Basic Select/Easy/Higher Than 75 Marks.sql
@@ -1,11 +1,12 @@
-/* Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID. */
+/* Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. 
+If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID. */
 
 
 SELECT NAME FROM STUDENTS
 WHERE MARKS > 75 
 ORDER BY SUBSTR(NAME, -3), ID ASC;
 
-/* This query returns the DISTINCT CITY from the specified table here STATION 
+/* This query returns the NAME from the specified table here STUDENTS
 WHERE clause is used to filter records/rows
 ORDER BY keyword is used to sort the result-set in ASC- ascending(by default) or DESC- descending order
 SUBSTR(string, start, length) function extracts a substring from a string (starting at any position).
