5345cbf70f853399a06621070ea5f68fe3826ca1
MaitriShah03 <108712031+MaitriShah03@users.noreply.github.com>
2024-02-22 14:19:43 +0530

    Delete SQL/Basic Select/Revising the Select query I.sql

--- SQL/Basic Select/Revising the Select query I.sql
+++
@@ -1,8 +0,0 @@
-/* This query selects all columns from 'CITY' table */
-SELECT * 
-/* Specifies the table from which to retreive the data */
-FROM CITY 
-/* Filter the rows to include those column where countrycode is 'USA' and population > 100000 */
-WHERE COUNTRYCODE = 'USA'
-/* AND if all the conditions to be TRUE */
-AND POPULATION > 100000;
