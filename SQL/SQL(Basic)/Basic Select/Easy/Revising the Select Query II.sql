6aabe41f8dd820566208ed3056726392dcba1202
MaitriShah03 <108712031+MaitriShah03@users.noreply.github.com>
2024-02-22 15:22:26 +0530

    Revising the Select Query II.sql
    Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.

--- SQL/SQL(Basic)/Basic Select/Easy/Revising the Select Query II.sql
+++ SQL/SQL(Basic)/Basic Select/Easy/Revising the Select Query II.sql
@@ -1,4 +1,4 @@
-/*Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA. */
+/* Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA. */
 
 
 /* This query selects NAME column from 'CITY' table */
