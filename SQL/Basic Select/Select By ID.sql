3e4e81b551ec7e9572ea4e87e2e00b80b6ddef16
MaitriShah03 <108712031+MaitriShah03@users.noreply.github.com>
2024-02-22 14:33:31 +0530

    Create Select By ID.sql
    Query all columns for a city in CITY with the ID 1661.

---
+++ SQL/Basic Select/Select By ID.sql
@@ -0,0 +1,6 @@
+/* This query selects all columns from 'CITY' table */
+SELECT *
+/* Specifies the table from which to retreive the data */
+FROM CITY 
+/* Filter the rows to include those column where countrycode is 'USA' and population > 120000 */
+WHERE ID = 1661;
