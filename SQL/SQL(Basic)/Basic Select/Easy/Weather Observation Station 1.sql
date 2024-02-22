c4a99d900ed9f5086e2730d4ffdf08bdb0a72cf5
MaitriShah03 <108712031+MaitriShah03@users.noreply.github.com>
2024-02-22 15:36:31 +0530

    Weather Observation Station 1.sql
    Query a list of CITY and STATE from the STATION table.

---
+++ SQL/SQL(Basic)/Basic Select/Easy/Weather Observation Station 1.sql
@@ -0,0 +1,7 @@
+/* Query a list of CITY and STATE from the STATION table. */
+
+
+/* This query selects CITY and STATE column from 'STATION' table */
+SELECT CITY, STATE
+/* Specifies the table from which to retreive the data */
+FROM STATION;
