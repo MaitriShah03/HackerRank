395d9c8111d1962a4af87a5dea5da1989999c2de
MaitriShah03 <108712031+MaitriShah03@users.noreply.github.com>
2024-02-22 15:24:39 +0530

    Select All.sql
    Query all columns (attributes) for every row in the CITY table.

---
+++ SQL/SQL(Basic)/Basic Select/Easy/Select All.sql
@@ -0,0 +1,7 @@
+/* Query all columns (attributes) for every row in the CITY table. */
+
+
+/* This query selects all columns/attributes for every row from 'CITY' table */
+SELECT *
+/* Specifies the table from which to retreive the data */
+FROM CITY;
