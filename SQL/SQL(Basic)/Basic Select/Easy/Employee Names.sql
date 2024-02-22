d4f23290a36807ef96f0919793ba7e9793702e15
MaitriShah03 <108712031+MaitriShah03@users.noreply.github.com>
2024-02-22 19:04:27 +0530

    Employee Names.sql
    Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.

---
+++ SQL/SQL(Basic)/Basic Select/Easy/Employee Names.sql
@@ -0,0 +1,9 @@
+/* Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order. */
+
+
+SELECT NAME FROM EMPLOYEE
+ORDER BY NAME ASC;
+
+/* This query returns the NAME column from the specified table here EMPLOYEE 
+WHERE clause is used to filter records/rows
+ORDER BY keyword is used to sort the result-set in ASC- ascending(by default) or DESC- descending order */
